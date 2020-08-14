4696d2f9af5bdbf0097f7695c1ddaf9a420411f6
Jancarlo Junior <34508489+juniorjrml@users.noreply.github.com>
2020-08-14 13:27:06 -0300

    Update aula05.py

--- aula05.py
+++ aula05.py
@@ -1,43 +1,25 @@
-
-"""m = [[1,2,3],[4,5,6],[7,8,9]]
+exercicios = ["xxx"]
+"""
+m = [[1,2,3],[4,5,6],[7,8,9]]
 print(m)
 m2 = m.copy()
 #m2.sort(reverse=True)
 [i.sort(reverse=True) for i in m2]
-print(m2)"""
+print(m2)
 
-"""
+
 def soma1_em_par(lista):
     return list(map(lambda x: x+1 if(x%2==0) else x,lista))
 l=[1,2,3]
 print(l)
 l = soma1_em_par(l)
 print(l)"""
-"""pessoas = [{"nome": "j1","idade": 11,"sexo": 'm'},{"nome": "j2","idade": 22,"sexo": 'f'}]
-
-campos = ["nome","idade","sexo"]
-
-def inserir_pessoas(base,lista,campos):
-    with open(base,"a") as f:
-        for pessoa in lista:
-            for campo in campos:
-                f.write("{} ".format(pessoa[campo]))
-            f.write("\n")
-
-def imprimir_mulher_maior_que(base,x,campos):
-    with open(base,"r") as f:
-        pessoas = f.readlines()
-    for p in pessoas:
-        aux = p.split()
-        if(int(aux[1])>x and aux[2]=="m"):
-            for campo in enumerate(campos):
-                print("{}: {}".format(campo[1],aux[campo[0]]))
-
-inserir_pessoas("ola.txt",pessoas,campos)
-imprimir_mulher_maior_que("ola.txt",1,campos)"""
 
 
-"""def matriz_transposta(matriz):
+
+
+"""
+def matriz_transposta(matriz):
     matriz_auxiliar = []
     for coluna in range(len(matriz[0])):
         nova_linha = []
@@ -59,10 +41,9 @@ for matriz in matrizes:
     mostrar_matriz(matriz)
     print("")
     mostrar_matriz(matriz_transposta(matriz))
-    print("")"""
+    print("")
 
 
-"""
 from functools import reduce
 import operator
 
@@ -83,3 +64,47 @@ def e_perfeito(x):
 print(e_perfeito(7))
 #qual o recurso para reduzir a quantidade de parametros
 """
+
+# _______________ inicio do exercicio xxx _______________
+
+
+def inserir_pessoas(base,lista_pessoas,caracteristicas):
+    with open(base, "a") as f:
+        for pessoa in lista_pessoas:
+            for caracteristica in caracteristicas:
+                f.write("{} ".format(pessoa[caracteristica]))
+            f.write("\n")
+
+
+def imprimir_mulher_maior_que(base, x, campos):
+    with open(base, "r") as f:
+        pessoas = f.readlines()
+    for p in pessoas:
+        caracteristicas = p.split()
+        if int(caracteristicas[1]) > x and caracteristicas[2] == "f":
+            for campo in enumerate(campos):
+                print("{}: {}".format(campo[1],caracteristicas[campo[0]]))
+            print("")
+def coleta_dados_pessoas(caracteristicas,numero_entradas):
+    lista_pessoas = []
+    for i in range(numero_entradas):
+        pessoa = {}
+        for caracteristica in caracteristicas:
+            pessoa[caracteristica] = input("Digite o {} ".format(caracteristica))
+        lista_pessoas.append(pessoa)
+    return lista_pessoas
+
+
+if "xxx" in exercicios:
+    print("Exercicio: implemente um programa que possua duas funcoes:")
+    print("(i) inserir conteudos de pessoas (nome, idade, sexo) em um arquivo texto")
+    print("(ii) imprimir o nome das mulheres com idade superior a x(dado de entrada)")
+    pessoas = [{"nome": "joao", "idade": 11, "sexo": 'm'},
+               {"nome": "maria", "idade": 22, "sexo": 'f'}]
+    campos = ["nome", "idade", "sexo"]
+    #pessoas = coleta_dados_pessoas(campos,3)
+
+    inserir_pessoas("pessoas.txt", pessoas, campos)
+    imprimir_mulher_maior_que("pessoas.txt", 4, campos)
+
+# _______________ fim do exercicio xxx _______________
