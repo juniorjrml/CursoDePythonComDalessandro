a47ba21a521558e904257779d4c630ce840c86f3
Jancarlo Junior <34508489+juniorjrml@users.noreply.github.com>
2020-08-19 13:45:42 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,84 +1,94 @@
-exercicio = [3]
-############# inicio so exercicio 1 ##################
-print("*"*20,"inicio do exercicio 1","*"*20)
-if(1 in exercicio):
-    numeros = []
-    qntNumeros = 2
-    for i in range(qntNumeros):
-        try:
-            auxiliar = float(input("digite um numero "))
-            numeros.append(auxiliar)
-        except:
-            print("Erro na leitura do numero")
-            numero = []
-            break
-    i = 0
-    j = 1
+exercicio = ["1_3"]
+
+
+def coletar_numero():
     try:
-        if numeros[i]>=numeros[j]:
-            print("primeiro é maior ou igual que o segundo")
+        return float(input("digite um numero "))
+    except ValueError:
+        print("Erro na leitura do numero(digite apenas numeros)")
+
+
+# -------------------- inicio so exercicio 1 --------------------
+if "1_1" in exercicio:
+    print("*" * 20, "inicio do exercicio 1", "*" * 20)
+    print("1. Ler dois números reais e informa se o primeiro é maior, menor ou")
+    print("igual ao segundo:\n\n")
+
+    numeros = []
+    qnt_numeros = 2
+    for i in range(qnt_numeros):
+        auxiliar = coletar_numero()
+        numeros.append(auxiliar)
+
+    try:
+        if numeros[0] > numeros[1]:
+            print("O primeiro numero digitado({}) é maior que o segundo numero digitado({})".format(numeros[0],
+                                                                                                    numeros[1]))
+        elif numeros[0] == numeros[1]:
+            print(
+                "O primeiro numero digitado({}) é igual ao segundo numero digitado({})".format(numeros[0], numeros[1]))
+        else:
+            print("O primeiro numero digitado({}) é menor que o segundo numero digitado({})".format(numeros[0],
+                                                                                                    numeros[1]))
     except:
-        print("Nao foi possivel comparar")
-else:
-    print("Exercicio nao solicitado!")
+        print("Nao foi possivel comparar :( \n tente novamente!")
 
-print("*"*20,"fim do exercicio 1","*"*20)
-############# fim so exercicio 1 ##################
+    print("*" * 20, "fim do exercicio 1", "*" * 20)
+# -------------------- fim so exercicio 1 --------------------
 
 
+# -------------------- inicio so exercicio 2 --------------------
 
-############# inicio so exercicio 2 ##################
-print("*"*20,"inicio do exercicio 2","*"*20)
-if(2 in exercicio):
+if "1_2" in exercicio:
+    print("*" * 20, "inicio do exercicio 2", "*" * 20)
+
+    print("2. Leia três strings e construa uma quarta string que representa a")
+    print("concatenação das três strings anteriores, separadas por espaço.")
+    print("Imprima a string construída.")
+
     textos = []
-    qntNumeros = 3
-    for i in range(qntNumeros):
+    qnt_palavras = 3
+    for i in range(qnt_palavras):
         try:
             auxiliar = input("digite uma string ")
             textos.append(auxiliar)
         except:
-            print("Erro na leitura da string" )
-            numero = []
+            print("Erro na leitura da string")
+            textos = []
             break
     texto = ''
     for i in textos:
-        texto += i+" "
+        texto += i + " "
 
     print(texto)
-else:
-    print("Exercicio nao solicitado!")
+    print("*" * 20, "fim do exercicio 2", "*" * 20)
+# -------------------- fim so exercicio 2 --------------------
 
-print("*"*20,"fim do exercicio 2","*"*20)
-############# fim so exercicio 2 ##################
+# -------------------- inicio so exercicio 3 --------------------
 
+if "1_3" in exercicio:
+    print("*" * 20, "inicio do exercicio 3", "*" * 20)
+    print("3. Leia um número inteiro x e imprima todos os seus divisores.")
+    print("a. Use 'while'")
+    print("b. Use 'for'\n\n")
 
-
-
-############# inicio so exercicio 3 ##################
-print("*"*20,"inicio do exercicio 3","*"*20)
-if(3 in exercicio):
-    numero = int(input("digite um numero: "))
+    numero = int(coletar_numero())
+    print("Usando while\n")
     divisores = []
-    for i in range(1,numero):
-        if((numero%i)==0):
+    for i in range(1, numero):
+        if (numero % i) == 0:
             divisores.append(i)
 
     print(divisores)
 
-
-
-    print("-"*10,"usando while ")
-    numeros = list(range(1,numero))
-    print(type(numeros))
+    print("Usando while\n")
+    numeros = list(range(1, numero))
     divisores = []
     while numeros:
         auxiliarInteiro = numeros.pop()
-        if((numero%auxiliarInteiro)==0):
+        if (numero % auxiliarInteiro) == 0:
             divisores.append(auxiliarInteiro)
 
     print(divisores)
-else:
-    print("Exercicio nao solicitado!")
-
-print("*"*20,"fim do exercicio 3","*"*20)
-############# fim so exercicio 3 ##################
+    print("*" * 20, "fim do exercicio 3", "*" * 20)
+# -------------------- fim so exercicio 3 --------------------
